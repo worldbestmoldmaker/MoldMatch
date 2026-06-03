0e5efb9fa19c8de7d3d2e49492414f5c99e997c4
worldbestmoldmaker <worldbestmoldmaker@gmail.com>
2026-06-03 06:14:27 -0400

    Update machine_app.py

--- machine_app.py
+++ machine_app.py
@@ -7,7 +7,7 @@ import time
 # ---------------------------
 # TITLE
 # ---------------------------
-st.markdown("# MoldMatch” Dark Blue)
+st.markdown("# MoldMatch”)
 
 # ---------------------------
 # LOAD DATA
