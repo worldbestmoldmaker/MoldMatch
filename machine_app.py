478f3364b2eef2e6a558024f96d042fde319925c
worldbestmoldmaker <worldbestmoldmaker@gmail.com>
2026-06-08 17:14:16 -0400

    Fix formatting of line shape width in machine_app.py

--- machine_app.py
+++ machine_app.py
@@ -553,7 +553,7 @@ if 'valid' in locals() and len(valid) > 0:
 # BOTTOM tie bar
     fig.add_shape(
         type="line",  
-        width=12
+        width=12,
         x0=0,
         y0=(platen_width - tie_bar_y) / 2 ,
         x1=platen_height,
