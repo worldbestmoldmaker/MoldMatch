c6b46107711f3705843bfb702ee063fe39ca7e90
worldbestmoldmaker <worldbestmoldmaker@gmail.com>
2026-06-09 15:27:34 -0400

    Fix indentation for print statement in machine_app.py

--- machine_app.py
+++ machine_app.py
@@ -569,7 +569,7 @@ if 'valid' in locals() and len(valid) > 0:
 # BOTTOM tie bar
     
     y0_value = (platen_width - tie_bar_y) / 2 - d
-print("y0 =", y0_value)
+    print("y0 =", y0_value)
 
     #fig.add_shape(
     fig.add_shape(
