947e2bd5056d5ce9b06f36739ed52b40fbaf29de
worldbestmoldmaker <worldbestmoldmaker@gmail.com>
2026-06-11 17:38:19 -0400

    Fix line shape x1 calculation in machine_app.py

--- machine_app.py
+++ machine_app.py
@@ -664,7 +664,7 @@ if 'valid' in locals() and len(valid) > 0:
         type="line",
         x0=(platen_x + tie_bar_x) / 2,
         y0=0,
-        x1=(platen_x + tie_bar_x) / 2 + d,
+        x1=(platen_x + tie_bar_x) / 2,
         y1=platen_open,
         line=dict(
             color="black",
